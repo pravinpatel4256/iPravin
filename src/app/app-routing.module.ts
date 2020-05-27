e33878ba299ac9810a1feeecd4c72504bb3e9b9b
Pravin <neelammpatel35@gmail.com>
2020-05-26 17:28:15 -0700

    Adding all page components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,25 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
 import {HomeComponent} from './components/home/home.component';
 import {ResumeComponent} from './components/resume/resume.component';
+import {AboutComponent} from './components/about/about.component';
+import {WorkComponent} from './components/work/work.component';
+import {ExperienceComponent} from './components/experience/experience.component';
+import {ContactComponent} from './components/contact/contact.component';
 
 
 const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'resume', component: ResumeComponent }
+  {path: '', component: HomeComponent},
+  {path: 'resume', component: ResumeComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'work', component: WorkComponent},
+  {path: 'experience', component: ExperienceComponent},
+  {path: 'contact', component: ContactComponent}
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,13 +9,22 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {CommonModule} from '@angular/common';
 import {FlexLayoutModule} from '@angular/flex-layout';
 import { ResumeComponent } from './components/resume/resume.component';
+import { AboutComponent } from './components/about/about.component';
+import { ExperienceComponent } from './components/experience/experience.component';
+import { WorkComponent } from './components/work/work.component';
+import { ContactComponent } from './components/contact/contact.component';
+import {MatCardModule} from '@angular/material/card';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    ResumeComponent
+    ResumeComponent,
+    AboutComponent,
+    ExperienceComponent,
+    WorkComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
@@ -23,7 +32,8 @@ import { ResumeComponent } from './components/resume/resume.component';
     BaseModule,
     BrowserAnimationsModule,
     CommonModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
